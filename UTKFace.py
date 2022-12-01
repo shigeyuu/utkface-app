18178a715f1a617863974d712286275d64b890d5
shigeyuu05 <shigeyuu05@gmail.com>
2022-12-01 18:22:28 +0900

    dvc

--- UTKFace.py
+++ UTKFace.py
@@ -45,7 +45,7 @@ def upload_file():
             # img = cv2.resize(img, (IM_HEIGHT,IM_WIDTH))
             
             #変換したデータをモデルに渡して予測する
-            result = model.predict(img)[0]
+            result = model.predict(img)[0][0]
             pred_answer =  str(result)  + "years old."
 
             return render_template("index.html",answer=pred_answer)
