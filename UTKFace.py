50f527a63c98205f84a5a4c3b79953dc9fa91a50
shigeyuu05 <shigeyuu05@gmail.com>
2022-12-01 18:23:20 +0900

    dvc

--- UTKFace.py
+++ UTKFace.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-model = load_model('./my_model.h5')#学習済みモデルをロード
+model = load_model('./data/my_model2.h5')#学習済みモデルをロード
 
 
 @app.route('/', methods=['GET', 'POST'])
